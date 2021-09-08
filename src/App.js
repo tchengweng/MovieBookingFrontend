3727735b4d3e4b2bf28fe06a70da9b8bbebcaef2
ChengWeng <90172920+tchengweng@users.noreply.github.com>
2021-09-08 14:08:52 +0800

    Test fetch on heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useCallback } from "react";
+// import React, { useState, useEffect, useCallback } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://movie-booking-backend-cw.herokuapp.com/AllMovies",
+        {
+          method: "GET",
+          mode: 'cors',
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+  
+      const data = await response.json();
+
+      console.log(data);
+  
+      // const loadedMovies = [];
+  
+      // for (const key in data) {
+      //   loadedMovies.push({
+      //     id: key,
+      //     title: data[key].title,
+      //     openingText: data[key].openingText,
+      //     releaseDate: data[key].releaseDate,
+      //   });
+      // }
+  
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  return <div className="App">Test app</div>;
 }
 
 export default App;
+
+
